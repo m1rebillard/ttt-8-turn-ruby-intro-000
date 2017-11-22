22fffc63de63b8a8c2dfe3d940a49911ac279b85
Myra Rebillard <myra.rebillard@gmail.com>
2017-11-22 03:36:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,34 @@
-def DISPLAY_BOARD (board)
+def display_board (board)
   puts " #{board[0]} |  #{board[1]}  #{board[2]} "
   puts " #{board[3]} |  #{board[4]}  #{board[5]} "
   puts " #{board[6]} |  #{board[7]}  #{board[8]} "
+end
+
+def valid_move (board, index)
+  if (index < 0 || index > 8)
+    return false
+  end
+  if (position_taken(board,index) == true)
+    return false
+  end
+  return true
+end
+
+def position_taken(board, index)
+  if (board[index] == "X" || board[index] == "O")
+    return true
+  end
+  return false
+end
+
+def input_to_index(input)
+  input -= 1
+  return input
+end
+
+def move (board, input, token)
+  index = input_to_index(input)
+  if (valid_move(board, index) == true)
+    board[index] = token
+  end
 end
